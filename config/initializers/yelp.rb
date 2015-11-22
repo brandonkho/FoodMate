7b19d63ee6d720d77c75a23d2f95cffefa77f5bb
unknown <brandonkho1@berkeley.edu>
2015-11-22 14:16:01 -0800

    added yelp initializer for API

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = 'vm_AU05ybFN0fbsqS7wvhQ'
+  config.consumer_secret = 'Ys2QNDzvdo9y8O9VboAWDIwiNqw'
+  config.token = 'kAJlIpKoxehjz4ai_37YzPIdbR1kcJAT'
+  config.token_secret = 'U_QJ4X9CDsCziGDDweu6EaB-r1o'
+end
